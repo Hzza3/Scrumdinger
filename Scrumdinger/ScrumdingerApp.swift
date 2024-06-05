bb009c58e02a5429f7ade7668bf83b24dc1231ee
Ahmed Hazzaa <ahazzaa@arabdt.com>
2024-06-05 11:23:47 +0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by A.Hazzaa on 05/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
